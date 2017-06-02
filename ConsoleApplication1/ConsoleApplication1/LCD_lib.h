669d122af4ad166a87b666898eae1621728b6ce8
PatrykMilewski <patryk.milewski@gmail.com>
2017-06-02 17:29:40 +0200

    working version, no bugs found

---
+++ ConsoleApplication1/ConsoleApplication1/LCD_lib.h
@@ -0,0 +1,105 @@
+﻿
+#define DEBUG
+
+#ifdef DEBUG
+
+int LCDWC, LCDWD, LCDRC, LCDRD;
+
+#else
+
+unsigned char xdata LCDWC _at_ 0xff80; //zapis rozkazow
+unsigned char xdata LCDWD _at_ 0xff81; //zapis danych
+unsigned char xdata LCDRC _at_ 0xff82; //odczyt stanu
+unsigned char xdata LCDRD _at_ 0xff83; //odczyt danych
+
+#endif // DEBUG
+
+
+void wait(unsigned int d) {
+	unsigned int i, j;
+	for (j = 0; j<d; j++)
+		for (i = 0; i<144; i++);
+}
+
+
+void write_control(unsigned char a) {	//zapis rozakazu, zapis danej
+	while (LCDRC & 0x80) wait(1);
+	LCDWC = a;
+}
+
+
+void write_data(unsigned char a) {	// zapis danej
+	while (LCDRC & 0x80) wait(1);
+	LCDWD = a;
+}
+
+
+/*
+*czyszczenie wyswietlacza
+*/
+void lcd_clr(void) {
+	write_control(0x01);
+	wait(10);
+}
+
+/*
+*inicjalizacja wyswietlacza
+*wywolac przed pierwszym uzyciem
+*/
+
+#ifdef DEBUG
+
+void lcd_init() {
+	return;
+}
+
+#else
+
+void lcd_init(void) {
+	unsigned char i;
+	static const unsigned char code init_tab[] = { 0x33, 0x32, 0x38, 0x08, 0x0f, 0x06 };
+	for (i = 0; i < sizeof init_tab; ++i) {
+		write_control(init_tab[i]);
+		wait(1);
+	}
+	lcd_clr();
+	write_control(0x80);            // wybierz adres DD RAM
+	wait(1);
+}
+
+#endif // DEBUG
+
+
+
+
+/*
+*wys³anie znaku na wyswietlacza
+*  zaimplementowano obsluge dwoch znakow kontrolnych
+*  LF (\n) - znak przejscia do nowej linii
+*  CR (\r) - znak powrotu karetki
+*/
+void lcd(unsigned char c) {
+	unsigned char x;
+	switch (c) {
+	case'\n':			//od nowego wiersza
+		x = LCDRC;
+		if (x <= 0x0f) write_control(0xc0);
+		if ((x >= 0x41) && (x <= 0x50)) {
+			write_control(0x80);
+		}
+		break;
+	case'\r':
+		x = LCDRC;
+		if ((x >= 0x00)&(x <= 0x0f))write_control(0x80);	//rozkaz dana
+		if ((x >= 0x41)&(x <= 0x50))write_control(0xc0);	//
+		break;
+	default:
+		if (c>0x1f && c<0x80) write_data(c);
+		wait(1);
+		break;
+	}
+	wait(1);
+	x = LCDRC;
+	if (x == 0x10)write_control(0xc0);			//rozkaz dana
+	if (x == 0x50)write_control(0x80);			//
+}
--- ConsoleApplication1/ConsoleApplication1/main.c
+++ ConsoleApplication1/ConsoleApplication1/main.c
@@ -1,22 +1,20 @@
 #include <stdlib.h>
 
-// wersja debug
+
 #define DEBUG
 
 #ifdef DEBUG
 
-int display;
-int *XBYTE = NULL;
-int P1_7;
+int P1_7, P1_5;
+int *XBYTE;
 
 #else
 
 #include <at89x52.h>
 #include <absacc.h>
+#include "LCD_lib.h"
 
-#endif // DEBUG
-
-#pragma region CONSTVALUES
+#endif
 
 #define TRUE 1
 #define FALSE 0
@@ -24,36 +22,48 @@ int P1_7;
 // adres rejestru CSKB1
 #define CSKB1 0xf022
 
+// adres rejestru CSKB0
+#define CSKB0 0xf021
+
 // kod klawisza aktwacji, do podawania sygnalow w alfabecie Morse'a
 #define KLAWISZAKTYWACJI 0x7F
 
-// kod znaku "-" myslinik
-#define MYSLNIK 0x40
+// kod klawisza 1
+#define KLAWISZJEDEN 0xFD
 
-// kod znaku "n" mala litera n
-#define LITERAN 0x54
+// kod klawisza 2
+#define KLAWISZDWA 0xFB
 
-// kod znaku "L" duza litera L
-#define LITERAL 0x38
+// czas w milisekundach, ile bedzie dzialal brzeczyk przy odczycie
+#define BRZECZYKCZAS 100
+
+// ile czasu bedzie brzeczal brzeczyk dla kropki
+#define BRZECZYKKROPKA 200
+
+// ile czasu bedzie brzeczal brzeczyk dla kreski
+#define BRZECZYKKRESKA 600
+
+// ile czasu bedzie trwala przerwa pomiedzy kropkami i kreskami
+#define BRZECZYKPRZERWA 200
+
+// ile czasu bedzie trwala przerwa pomiedzy znakami w alfabecie
+#define BRZECZYKPRZERWADLUGA 600
 
 // minimalny czas w ms na krotki znak w alfabecie Morse'a
-#define shortMinTime 100
+#define shortMinTime 50
 
 // maksymalny czas w ms na krotki znak w alfabecie Morse'a
-#define shortMaxTime 500
+#define shortMaxTime 400
 
 // minimalny czas w ms na dlugi znak w alfabecie Morse'a
-#define longMinTime 500
+#define longMinTime 400
 
 // maksymalny czas w ms na dlugi znak w alfabecie Morse'a
-#define longMaxTime 2000
+#define longMaxTime 1200
 
 // stala okreslajaca ile razy musi wykonac sie petla for
 // zeby opoznic program o 1 milisekunde
 #define oneMilisecondConst 6
-
-// okresla, ile mozemy wyswietlic maksymalnie cfyr
-#define digitsAmount 6
 
 // okresla, ile jest liter w alfabecie
 #define lettersAmount 26
@@ -61,292 +71,401 @@ int P1_7;
 // okresla, ile jest cyfr mozliwych roznych
 #define numbersAmount 10
 
-// czas w milisekundach okreslajacy, co ile przelaczamy sie
-// pomiedzy cyframi w wyswietlaczu 7 segmentowym
-// mozna dostosowac jak jest efekt mrugania
-#define displayNumberDelay 10	
+// okresla, na ilu maksymalnie znakach kodujemy
+// cyfry w alfabecie Morse'a
+#define maxNumbersMorse 5
 
-// czas w milisekundach potrzebny do wyswietlenia
-// czasu reakcji na wyswietlaczu 7 segmentowym
-#define displayValueDelay (digitsAmount * displayNumberDelay)
-
-// opoznienie pomiedzy wyswietlaniem cyfr na wyswietlaczu 7 segementowym
-// tez okresla dlugosc wyswietlania jednej cyfry
-#define displayDigitDelay 100
+// okresla, na ilu maksymalnie znakach kodujemy
+// litery w alfabecie Morse'a
+#define maxLettersMorse 4
 
 // stala okreslajaca ile czasu wykonywala sie petla,
 // ktora mierzy ile czasu wciskano klawisz aktywacji
-#define loopTimeLength (oneMilisecondConst / 2)
-
-// czas w milisekundach okreslajacy, ile czasu bedzie wyswietlany
-// wynik pomiaru czasu reakcji na wyswietlaczu
-#define displaySymbolTimePeriod 5000
-
-#pragma endregion
-
-// kody cyfr na wyswietlaczu 7 segmentowym
-const char numbers7seg[numbersAmount] = { 
-	0x3F,	// 0
-	0x06,	// 1
-	0x5B,	// 2
-	0x4F,	// 3
-	0x66,	// 4
-	0x6D,	// 5
-	0x7D,	// 6
-	0x07,	// 7
-	0x7F,	// 8
-	0x6F 	// 9
-};
-
-// kody liter na wyswietlaczu 7 segmentowym
-const char letters7seg[lettersAmount] = {
-	0x77,	// a
-	0x7c,	// b
-	0x39,	// c
-	0x5e,	// d
-	0x79,	// e
-	0x71,	// f
-	0x6f,	// g
-	0x76,	// h
-	0x30,	// i
-	0x1e,	// j
-	0x74,	// k
-	0x38,	// l
-	0x15,	// m
-	0x54,	// n
-	0x3f,	// o
-	0x73,	// p
-	0x67,	// q
-	0x50,	// r
-	0x6d,	// s
-	0x78,	// t
-	0x3e,	// u
-	0x1c,	// v
-	0x2a,	// w
-	0x68,	// x
-	0x6e,	// y
-	0x5b	// z
-};
+#define loopTimeLength 44
 
 // litery zakodowane w kodzie Morse'a
-const char letters[lettersAmount][5] = {
-	{ -1, -1, -1, 1, 0 },   //A
-	{ -1, 0, 1, 1, 1 },		//B
-	{ -1, 0, 1, 0, 1 },		//C
-	{ -1, -1, 0, 1, 1 },	//D
-	{ -1, -1, -1, -1, 1 },	//E
-	{ -1, 1, 1, 0, 1 },		//F
-	{ -1, -1, 0, 0, 1 },	//G
-	{ -1, 1, 1, 1, 1 },		//H
-	{ -1, -1, -1, 1, 1 },	//I
-	{ -1, 1, 0, 0, 0 },		//J
-	{ -1, -1, 0, 1, 0 },	//K
-	{ -1, 1, 0, 1, 1 },		//L
-	{ -1, -1, -1, 0, 0 },	//M
-	{ -1, -1, -1, 0, 1 },	//N
-	{ -1, -1, 0, 0, 0 },	//O
-	{ -1, 1, 0, 0, 1 },		//P
-	{ -1, 0, 0, 1, 0 },		//Q
-	{ -1, -1, 1, 0, 1 },	//R
-	{ -1, -1, 1, 1, 1 },	//S
-	{ -1, -1, -1, -1, 0 },	//T
-	{ -1, -1, 1, 1, 0 },	//U
-	{ -1, 1, 1, 1, 0 },		//V
-	{ -1, -1, 1, 0, 0 },	//W
-	{ -1, 0, 1, 1, 0 },		//X
-	{ -1, 0, 1, 0, 0 },		//Y
-	{ -1, 0, 0, 1, 1 },		//Z
+// bity ustawione na:
+// 10 = brak znaku, 01 = kreska, 00 = kropka
+char letters[lettersAmount] = {
+	0xa4, //A 	10 10  01 00
+	0x01,	//B 	00 00  00 01
+	0x11,	//C 	00 01  00 01
+	0x81,	//D 	10 00  00 01
+	0xa8,	//E 	10 10  10 00
+	0x10,	//F 	00 01  00 00
+	0x85,	//G 	10 00  01 01
+	0x00,	//H 	00 00  00 00
+	0xa0,	//I 	10 10  00 00
+	0x54,	//J 	01 01  01 00
+	0x91,	//K 	10 01  00 01
+	0x04,	//L 	00 00  01 00
+	0xa5,	//M 	10 10  01 01
+	0xa4,	//N 	10 10  01 00
+	0x95,	//O 	10 01  01 01
+	0x14,	//P 	00 01  01 00
+	0x45,	//Q 	01 00  01 01
+	0x84,	//R 	10 00  01 00
+	0x80,	//S 	10 00  00 00
+	0xa9,	//T 	10 10  10 01
+	0x90,	//U 	10 01  00 00
+	0x40,	//V 	01 00  00 00
+	0x94,	//W 	10 01  01 00
+	0x41,	//X 	01 00  00 01
+	0x51,	//Y 	01 01  00 01
+	0x05,	//Z 	00 00  01 01
 };
 
 // cyfry zakodowane w kodzie Morse'a
-const char numbers[numbersAmount][5] = {
-	{ 0, 0, 0, 0, 0 },		//0
-	{ 1, 0, 0, 0, 0 },		//1
-	{ 1, 1, 0, 0, 0 },		//2
-	{ 1, 1, 1, 0, 0 },		//3
-	{ 1, 1, 1, 1, 0 },		//4
-	{ 1, 1, 1, 1, 1 },		//5
-	{ 0, 1, 1, 1, 1 },		//6
-	{ 0, 0, 1, 1, 1 },		//7
-	{ 0, 0, 0, 1, 1 },		//8
-	{ 0, 0, 0, 0, 1 },		//9
+// bity ustawione na:
+// 1 = kreska, 0 = kropka
+// 3 najstarsze bity sa nieistotne
+const char numbers[numbersAmount] = {
+	0x1f,	//0 	000 1  1 1 1 1
+	0x1e,	//1 	000 1  1 1 1 0
+	0x1c,	//2 	000 1  1 1 0 0
+	0x18,	//3 	000 1  1 0 0 0
+	0x10,	//4 	000 1  0 0 0 0
+	0x00,	//5 	000 0  0 0 0 0
+	0x01,	//6 	000 0  0 0 0 1
+	0x03,	//7 	000 0  0 0 1 1
+	0x07,	//8 	000 0  0 1 1 1
+	0x0f,	//9 	000 0  1 1 1 1
 };
 
-// funkcja pauzujaca program
-void delayProgram(unsigned long int value) {
-	unsigned long int i = 0;
+// zamiana tablicy inputCode na kod odpowiadajacy
+// wartosci danej litery w alfabecie Morsea
+char compressLetter(char *inputCode) {
+	char i;
 
-	// dostosowanie, zeby dla value = 1000, opoznic program o 1000 milisekund
-	// dobrane metoda prob i bledow
-	value *= 0.95;
+	// ustawiamy wszystkie bity w result na 0
+	char result = 0;
 
-	// petla opozniajaca
-	for (; i < value * oneMilisecondConst; i++);
-}
+	// petla zamieniajaca ciag kresek i kropek na wartosc
+	// zaczynamy od i = maxLettersMorse, gdyz pozniej bedziemy
+	// przesuwac bity w result w lewo, dlatego najpierw musimy
+	// zajac sie najbardziej znaczacymi znakami w inputCode
+	for (i = maxLettersMorse; i >= 0; i--) {
 
+		// jezeli kropka, to zmieniamy najmlodszy bit w result na 1
+		if (inputCode[i] == 1)
+			result += 1;
 
-// wyswietlenie litery/cyfry na wyswietlaczu 7 segemntowym
-// wyswietlanie wyglada tak: L-A / n-9
-// gdzie L/n oznacza "litera"/"muber", A/9 dowolona litere/cyfre
-void display7Seg(char value, char *codeTable, char type) {
-	unsigned int j;
-	char displayTable[3], temp, i;
+		// jezeli kreska, to zmieniamy 2 najmlodsze bity w result na 10
+		else if (inputCode[i] == 2)
+			result += 2;
 
-	// jezeli litera
-	if (value >= 65)
-		value -= 65;
-	// jezeli nie litera (cyfra)
-	else
-		value -= 48;
+		// jezeli nie wejdzie w zaden warunek, to wartosc bitu sie nie zmienia (zostaje 0)
 
-	for (i = 0; i < 3; i++) {
-		// wyswietlenie litery/cyfry
-		if (i == 0)
-			temp = codeTable[value];
-
-		// wyswietlenie myslnika
-		else if (i == 1)
-			temp = MYSLNIK;
-
-		// wyswietlenie litery "L" / "n" - typ znaku
-		else if (i == 2)
-			temp = type;
-
-		displayTable[i] = temp;
+		// jezeli ostatni obieg petli, to nie musimy juz przesuwac 
+		// results bitowo, gdyz skonczylismy prace
+		if (i != 0)
+			// przesuniecie bitowe o 2 bity, gdyz mamy 3 stany -> 00, 01, 10, po dopisaniu
+			// kazdego trzeba przesunac o 2 bity w lewo, zeby zachowac poprawnosc kodowania
+			result = result << 2;
 	}
 
-	for (i = 0, j = 0; j < displaySymbolTimePeriod; j++, i++) {
-		i = i % 3;
-
-		XBYTE[0xF030] = 1 << i;
-		XBYTE[0xF038] = displayTable[i];
-
-		display = 0;
-
-		// opoznienie, zeby sie przez chwile wyswietlala dana cyfra na wyswietlaczu
-
-		for (j = 0; j < displayDigitDelay; j++);
-
-		display = 1;
-
-		// opoznienie pomiedzy cyframi na wyswietlaczu
-		for (j = 0; j < displayDigitDelay; j++);
-	}
+	return result;
 }
 
-// NOT TESTED
-//
+// funkcja analogiczna do compressLetter
+char compressNumber(char *inputCode) {
+	char i, result = 0;
+
+	for (i = maxNumbersMorse - 1; i >= 0; i--) {
+
+		if (inputCode[i] == 1)
+			result += 1;
+
+		if (i != 0)
+			result = result << 1;
+	}
+
+	return result;
+}
+
 // tlumaczy kod w alfabecie Morse'a na literke
-char translateToChar(char code[5]) {
-	char match, characterCode;
+char translateToChar(char *inputCode) {
+	char compressedCode, i;
 
 	// jezeli jest litera
-	if (code[4] == -1) {
-		for (char i = 0; i < lettersAmount; i++) {
-			match = 1;
-			for (char j = 0; j < 4; j++) {
+	if (inputCode[4] == 2) {
 
-				// sprawdzamy, czy kolejne znaki w kodzie Morse'a sie zgadzaja
-				if (code[j] != letters[i][j])
-					// jezeli nie, to mamy inny znak i ustawiamy match na 0
-					match = 0;
-			}
+		// przeliczamy kropki i kreski na konkretna wartosc funkcja kompresujaca
+		compressedCode = compressLetter(inputCode);
 
-			// jezeli znalezlismy kod naszej litery, zwracamy jej kod ASCII
-			if (match == 1)
+		// szukamy odpowiadajacej wartosci w zbiorze liter
+		for (i = 0; i < lettersAmount; i++) {
+
+			// jezeli znajdziemy taka sama wartosc, to znaczy ze znalezlismy nasza litere
+			// i trzeba ja zwrocic, litery zaczynaja sie od 65 w ASCII, dlatego tyle dodajemy
+			// zmienna "i" okresla polozenie w tablicy letters
+			if (letters[i] == compressedCode)
 				return i + 65;
 		}
 	}
+
 	// jezeli nie jest litera (cyfra)
 	else {
-		for (char i = 0; i < numbersAmount; i++) {
-			match = 1;
-			for (char j = 0; j < 4; j++) {
-				
-				// sprawdzamy, czy kolejne znaki w kodzie Morse'a sie zgadzaja
-				if (code[j] != numbers[i][j])
-					// jezeli nie, to mamy inny znak i ustawiamy match na 0
-					match = 0;
-			}
 
-			// jezeli znalezlismy kod naszej cyrfy, zwracamy jej kod ASCII
-			if (match == 1)
+		// analogicznie jak dla liter
+		compressedCode = compressNumber(inputCode);
+
+		for (i = 0; i < numbersAmount; i++) {
+			if (numbers[i] == compressedCode)
 				return i + 48;
 		}
 	}
 
-	// jezeli bledny kod wejsciowy, zwraca kod litery 'X'
-	return letters[23];
+	// jezeli bledny kod wejsciowy, zwraca 0
+	return 0;
 }
 
-// NOT TESTED
-//
 // tlumaczy litere/cyfre na kod w alfabecie Morse'a
-char * translateToMors(char character, char **codeTable) {
-
-	// tablica w ktorej bedzie zakodowany znak
-	char *result = malloc(sizeof(char) * 5);
-	
+char getMorseCode(char input) {
 	// jezeli litera
-	if (character >= 65)
-		character -= 65;
+	if (input >= 65) {
+		input -= 65;
+		return letters[input];
+	}
 	// jezeli nie litera (cyfra)
-	else
-		character -= 48;
-
-	// przepisanie z tablicy z kodami Morse'a kodu znaku do tablicy wynikowej
-	for (char i = 0; i < 5; i++)
-		result[i] = codeTable[character][i];
-
-	// zwracamy zakodowany znak
-	return result;
+	else {
+		input -= 48;
+		return numbers[input];
+	}
 }
 
-int main() {
-	loopTimeLength;
-	unsigned int time;
-	char character[5];
+// funkcja sygnalizujaca, ze nie rozpoznano znaku w alfabecie Morse'a
+// za pomoca diody TEST
+void failedToTranslate() {
+	char i;
+
+	// dwa razy mruga dioda
+	for (i = 0; i < 2; i++) {
+		P1_7 = 0;
+		wait(500);
+		P1_7 = 1;
+		wait(500);
+	}
+}
+
+// funkcja sygnalizujaca pomyslne odczytanie jednego znaku w alfabecie Morse'a
+// za pomoca brzeczyka
+// 1 brzekniecie = kropka, 2 brzekniecia = kreska, 3 brzekniecia = koniec znaku
+void successfullyRead(int input) {
+	char i;
+
+	// w zaleznosci od podanej wartosci input, tyle razy wlaczy i wylaczy brzeczyk
+	for (i = 0; i < input + 1; i++) {
+		P1_5 = 0;
+		wait(BRZECZYKCZAS);
+		P1_5 = 1;
+		wait(BRZECZYKCZAS);
+	}
+}
+
+void toNormalAlphabet() {
+	char character[5], input, i;
+	unsigned long int time;
 
 	// petla glowna programu
 	while (TRUE) {
 
-		// petla rozpoznajaca caly znak (maksymalnie 5 sygnalow w alfabecie Morse'a)
-		for (char i = 0; i < 5; i++) {
+		// zerowanie tablicy do wartosci 2, ktore interpetujemy 
+		// jako ani kropka ani kreska, po prostu pusty znak
+		for (i = 0; i < 5; i++) {
+			character[i] = 2;
+		}
 
-			//zerowanie tablicy z rozpoznanymi sygnalami
-			character[i] = -1;
-			
-			//liczy czas po jakim nacisnieto przycisk
+		// petla rozpoznajaca caly znak (maksymalnie 5 sygnalow w alfabecie Morse'a)
+		for (i = 0; i < 5; i++) {
+
+			// zerowanie licznika czasu
 			time = 0;
 
-			// petla od rozpoznawania jednego konkretnego sygnalu (dlugi/krotki)
+			// petla czekajaca na nacisniecie klawisza
 			while (TRUE) {
-
-				// jezeli nacisnieto klawisz aktywacji, przerywamy petle i mamy czas nacisniecia przycisku
+				// jezeli nacisnieto klawisz aktywacji, przerywamy petle
 				if (XBYTE[CSKB1] == KLAWISZAKTYWACJI)
 					break;
+			}
+
+			// liczymy ile czasu wcisniety jest klawisz
+			while (TRUE) {
+				// jezeli klawisz nie jest juz wciskany, przerywamy petle
+				if (XBYTE[CSKB1] != KLAWISZAKTYWACJI)
+					break;
+
 				time++;
 			}
 
-			// obliczamy po jakim czasie w ms nacisnieto przycisk
-			time = time / loopTimeLength;
+			// obliczamy dlugosc naciskania przycisku
+			time /= loopTimeLength;
 
-			// czy czas nacisniecia zawiera sie w przedziale czasowym krotkiego znaku
-			if (time >= shortMinTime && time < shortMaxTime)
+			// czy czas nacisniecia zawiera sie w przedziale czasowym kropki
+			if (time >= shortMinTime && time < shortMaxTime) {
 				character[i] = 0;
+				successfullyRead(0);
+			}
 
-			// czy czas nacisniecia zawiera sie w przedziale czasowym dlugiego znaku
-			if (time >= longMinTime && time < longMaxTime)
+			// czy czas nacisniecia zawiera sie w przedziale czasowym kreski
+			if (time >= longMinTime && time < longMaxTime) {
 				character[i] = 1;
+				successfullyRead(1);
+			}
 
 			// czy czas nisniecia jest dluzszy niz maksymalny czas dlugiego znaku
 			// jezeli tak, to znaczy, ze koniec podawania znaku
-			if (time >= longMaxTime)
+			if (time >= longMaxTime) {
+				successfullyRead(2);
 				break;
+			}
 		}
 
+		// wczytalismy znak od uzytkownika, teraz go rozpoznamy
 
+		// translacja z morsa na chara
+		input = translateToChar(character);
+
+		// jezeli bledny kod - nie rozpoznano znaku
+		if (input == 0)
+			failedToTranslate();
+
+		// jezeli poprawnie rozpoznano znak, wyswietlamy go na lcd
+		else
+			lcd(input);
+	}
+}
+
+void toMorseAlphabet(char *text) {
+	unsigned int i;
+	char charCode, last2BitsMask = 0x03, last2Bits, j;
+
+	for (i = 0; text[i] != NULL; i++) {
+		charCode = getMorseCode(text[i]);
+
+		// jezeli jest cyfra
+		if (text[i] < 65) {
+			for (j = 0; j < 5; j++) {
+
+				// jezeli kropka
+				if (charCode % 2 == 0) {
+					P1_5 = 0;
+					wait(BRZECZYKKROPKA);
+				}
+
+				//jezeli kreska
+				else {
+					P1_5 = 0;
+					wait(BRZECZYKKRESKA);
+				}
+
+				P1_5 = 1;
+
+				// przerwa pomiedzy kropkami/kreskami
+				wait(BRZECZYKPRZERWA);
+
+				// przesuwamy bitowo w prawo, gdyz kazda kropka/kreska zapisana jest
+				// na jednym bicie i po odczytaniu jej, musimy przesunac calosc w prawo
+				// 0 0 0 1  0 1 0 1 >> 1 = x 0 0 0  1 0 1 0
+				charCode = charCode >> 1;
+			}
+
+			// przerwa pomiedzy znakami
+			wait(BRZECZYKPRZERWADLUGA);
+		}
+
+		// jezeli jest litera
+		else {
+			for (j = 0; j < 4; j++) {
+
+				last2Bits = charCode & last2BitsMask;
+
+				// jezeli kropka
+				if (last2Bits == 0) {
+					P1_5 = 0;
+					wait(BRZECZYKKROPKA);
+				}
+
+				// jezeli kreska
+				else if (last2Bits == 1) {
+					P1_5 = 0;
+					wait(BRZECZYKKRESKA);
+				}
+
+				//jezeli koniec znaku
+				else
+					break;
+
+				P1_5 = 1;
+
+				// przerwa pomiedzy kropkami/kreskami
+				wait(BRZECZYKPRZERWA);
+
+				// przesuwamy bitowo w prawo, gdyz kazda kropka/kreska zapisana jest
+				// na dwoch bitach i po odczytaniu jednej, musimy przesunac calosc w prawo
+				// 10 01 00 10 >> 2 = xx 10 01 00
+				charCode = charCode >> 2;
+			}
+
+			// przerwa pomiedzy literami
+			wait(BRZECZYKPRZERWADLUGA);
+		}
+	}
+}
+
+void wyswietlLCD(char *text, char noDelayClear) {
+	char i;
+
+	lcd_clr();
+
+	for (i = 0; text[i] != NULL; i++) {
+		lcd(text[i]);
 	}
 
-	return 0;
+	// jezeli noDelayClear == 1, to nie opozniamy programu i nie czyscimy lcd
+	if (noDelayClear == 1)
+		return;
+
+	for (i = 0; i < 3; i++) {
+		wait(1000);
+	}
+
+	lcd_clr();
+}
+
+void main() {
+	char text[] = "123";
+	char flag = 0;
+
+	lcd_init();
+
+	while (TRUE) {
+
+		wyswietlLCD("Wybierz tryb\npracy 1 lub 2", 1);
+
+		while (TRUE) {
+
+			// alfabet Morse'a -> zwykly alfabet
+			if (XBYTE[CSKB0] == KLAWISZJEDEN) {
+				wyswietlLCD("Wybrano\nMorse -> normal", 0);
+				flag = 1;
+				break;
+			}
+
+			// zwykly alfabet -> alfabet Morse'a
+			if (XBYTE[CSKB0] == KLAWISZDWA) {
+				wyswietlLCD("Wybrano\nnormal -> Morse", 0);
+				flag = 2;
+				break;
+			}
+		}
+
+		// w zaleznosci od wybranego trybu, wywolujemy odpowiednia funkcje
+		if (flag == 1)
+			toNormalAlphabet();
+
+		else if (flag == 2)
+			toMorseAlphabet(text);
+
+	}
 }
